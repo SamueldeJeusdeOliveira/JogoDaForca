2c6e3c2d079fd3bd2bf148aa3bddd1881404792c
Samuel <Samuel@DESKTOP-EEN6AIH>
2025-03-25 19:19:54 -0300

    Adicionar arquivos de projeto.

---
+++ JogoDaForca/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace JogoDaForca
+{
+    class Program
+    {
+        //Versão 1: Estrutura básica e entrada do usuário
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("----------------------------------------------");
+
+
+            }
+        }
+    }
+}
